c677001ca190b7d33f3f98e2fd7b1963d1bc4965
Jonathan008-bit <jonathanriquelme96@gmail.com>
2026-04-30 18:36:01 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import OpenAI from "openai";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const openai = new OpenAI({
+  apiKey: "18/03/2026",
+});
+
+// prueba
+app.get("/", (req, res) => {
+  res.send("Backend funcionando 🚀");
+});
+
+// IA
+app.post("/process", async (req, res) => {
+  const { text, mode } = req.body;
+
+  let prompt = "";
+
+  if (mode === "summary") {
+    prompt = `Resume este texto:\n${text}`;
+  }
+
+  if (mode === "learn") {
+    prompt = `Explícame este contenido paso a paso:\n${text}`;
+  }
+
+  if (mode === "quiz") {
+    prompt = `Crea 5 preguntas tipo test con 4 alternativas y marca la correcta:\n${text}`;
+  }
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4.1",
+      messages: [{ role: "user", content: prompt }],
+    });
+
+    res.json({
+      result: response.choices[0].message.content,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error en IA" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor activo");
+});
